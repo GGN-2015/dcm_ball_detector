9fb62fb7ca77b6d0a06d6f6123a81a3f188b28c6
GGN-2015 <premierbob@qq.com>
2024-09-07 23:21:59 +0800

    feat:large_ball_rate_check

--- dcm_ball_detector/__init__.py
+++ dcm_ball_detector/__init__.py
@@ -1,8 +1,11 @@
 from .advanced_box_method      import get_all_cluster_center_in_folder
 from .advanced_box_method_test import svm_get_ball_centers_in_folder_and_dump_log
+from . import os_interface
 
 # 根据所有的 dcm 影像获取识别到的所有标志物的坐标
 __all__ = [
     "get_all_cluster_center_in_folder",
     "svm_get_ball_centers_in_folder_and_dump_log"
 ]
+
+os_interface.clear_log_image() # 清空日志文件夹
